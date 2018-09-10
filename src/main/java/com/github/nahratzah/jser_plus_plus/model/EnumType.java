288693fe861f235d85bbd383db32d1f930c849ea
Ariane van der Steldt <ariane@stack.nl>
2018-09-10 15:21:45 +0100

    Implement enum values.

--- src/main/java/com/github/nahratzah/jser_plus_plus/model/EnumType.java
+++ src/main/java/com/github/nahratzah/jser_plus_plus/model/EnumType.java
@@ -3,10 +3,14 @@ package com.github.nahratzah.jser_plus_plus.model;
 import com.github.nahratzah.jser_plus_plus.config.Config;
 import com.github.nahratzah.jser_plus_plus.input.Context;
 import java.util.Arrays;
+import java.util.Collection;
+import static java.util.Collections.unmodifiableList;
+import java.util.List;
 import java.util.Locale;
-import java.util.Map;
-import java.util.function.Function;
+import java.util.Objects;
+import static java.util.Objects.requireNonNull;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * An enum type.
@@ -14,6 +18,8 @@ import java.util.stream.Collectors;
  * @author ariane
  */
 public class EnumType extends ClassType {
+    private static final List<String> EXTRA_PUBLIC_INCLUDES = unmodifiableList(Arrays.asList("string_view"));
+
     public EnumType(Class<?> c) {
         super(c);
         if (!c.isEnum())
@@ -23,6 +29,12 @@ public class EnumType extends ClassType {
     @Override
     public void init(Context ctx, Config cfg) {
         super.init(ctx, cfg); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<String> getIncludes(boolean publicOnly) {
+        return Stream.concat(super.getIncludes(publicOnly).stream(), EXTRA_PUBLIC_INCLUDES.stream())
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -36,11 +48,12 @@ public class EnumType extends ClassType {
      * @return Map containing as key the original name of the enum constant,
      * mapped to the remapped name.
      */
-    public Map<String, String> getEnumValues() {
+    public Collection<EnumConstant> getEnumValues() {
         return Arrays.stream(c.getEnumConstants())
                 .map(Enum.class::cast)
                 .map(e -> e.name())
-                .collect(Collectors.toMap(Function.identity(), EnumType::remapEnumConstant));
+                .map(EnumConstant::new)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -51,5 +64,41 @@ public class EnumType extends ClassType {
      */
     private static String remapEnumConstant(String name) {
         return name.substring(0, 1).toUpperCase(Locale.ROOT) + name.substring(1).toLowerCase(Locale.ROOT);
+    }
+
+    /**
+     * Track enum names and their remapped name.
+     */
+    public static final class EnumConstant {
+        public EnumConstant(String name) {
+            this.name = requireNonNull(name);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getRemappedName() {
+            return remapEnumConstant(name);
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 7;
+            hash = 37 * hash + Objects.hashCode(this.name);
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null) return false;
+            if (getClass() != obj.getClass()) return false;
+            final EnumConstant other = (EnumConstant) obj;
+            if (!Objects.equals(this.name, other.name)) return false;
+            return true;
+        }
+
+        private final String name;
     }
 }
