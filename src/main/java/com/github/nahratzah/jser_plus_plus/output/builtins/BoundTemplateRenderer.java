cfc8ddf6c157154d14fcf6ca44dc7e0bb819b841
Ariane van der Steldt <ariane@stack.nl>
2018-09-16 19:59:05 +0100

    Document BoundTemplateRenderer.

--- src/main/java/com/github/nahratzah/jser_plus_plus/output/builtins/BoundTemplateRenderer.java
+++ src/main/java/com/github/nahratzah/jser_plus_plus/output/builtins/BoundTemplateRenderer.java
@@ -23,6 +23,16 @@ public class BoundTemplateRenderer implements AttributeRenderer {
     private static final Logger LOG = Logger.getLogger(BoundTemplateRenderer.class.getName());
     public static final Class<BoundTemplate> ATTRIBUTE_CLASS = BoundTemplate.class;
 
+    /**
+     * Render a {@link BoundTemplate}.
+     *
+     * @param o An instance of {@link BoundTemplate}.
+     * @param formatString A string containing the configuration of the
+     * renderer.
+     * @param locale Locale in which to render. This argument is unused.
+     * @return Rendered form of the {@link BoundTemplate}, according to options
+     * specified in the format string.
+     */
     @Override
     public String toString(Object o, String formatString, Locale locale) {
         LOG.log(Level.FINEST, "Invoking {0}(Object={1}, formatString={2})", new Object[]{this.getClass(), o, formatString});
@@ -32,17 +42,37 @@ public class BoundTemplateRenderer implements AttributeRenderer {
         return tmpl.visit(config.style.apply(config));
     }
 
+    /**
+     * Configuration of the {@link BoundTemplateRenderer}.
+     */
     private static class Config {
+        /**
+         * Create a {@link Style#TYPE} configuration, with specified accessor
+         * base.
+         *
+         * @param base The base of an accessor.
+         * @return Configuration that renders types.
+         */
         public static Config typeWithBase(String base) {
-            Config config = new Config(Style.TYPE);
+            final Config config = new Config(Style.TYPE);
             config.base = base;
             return config;
         }
 
+        /**
+         * Construct a configuration with the given style.
+         *
+         * @param style A configuration style.
+         */
         private Config(Style style) {
             this.style = requireNonNull(style);
         }
 
+        /**
+         * Populate configuration from format string.
+         *
+         * @param formatString A format string used by the template engine.
+         */
         public Config(String formatString) {
             if (formatString.isEmpty()) return;
 
@@ -58,11 +88,32 @@ public class BoundTemplateRenderer implements AttributeRenderer {
             LOG.log(Level.FINEST, "Parsed format string \"{0}\" into {1}", new Object[]{formatString, this});
         }
 
+        /**
+         * Style of decoding.
+         */
         public Style style = Style.TYPE;
+        /**
+         * If the style is {@link Style#TYPE}, create class types instead of
+         * generics.
+         */
         public boolean classType = false;
+        /**
+         * If the style is {@link Style#TYPE}, create accessor types instead of
+         * generics.
+         */
         public boolean accessorType = false;
+        /**
+         * If rendering accessors, use this as the base template argument to the
+         * accessor.
+         */
         public String base;
 
+        /**
+         * Apply configuration key-value item.
+         *
+         * @param key The key of the configuration option.
+         * @param value The value of the configuration option.
+         */
         private void apply(String key, String value) {
             if (Objects.equals(key, "style"))
                 style = Style.valueOf(value.toUpperCase(Locale.ROOT));
@@ -72,6 +123,11 @@ public class BoundTemplateRenderer implements AttributeRenderer {
                 throw new IllegalArgumentException("Invalid key \"" + key + "\"");
         }
 
+        /**
+         * Apply configuration item.
+         *
+         * @param key A no-argument configuration option.
+         */
         private void apply(String key) {
             if (Objects.equals(key, "class"))
                 classType = true;
@@ -110,169 +166,193 @@ public class BoundTemplateRenderer implements AttributeRenderer {
         }
     }
 
+    /**
+     * Render style information.
+     */
     private static enum Style implements Function<Config, BoundTemplate.Visitor<String>> {
+        /**
+         * Render style for types. The entire {@link BoundTemplate} will be
+         * evaluated.
+         */
         TYPE(TypeVisitor::new),
+        /**
+         * Render style for tags. Only the root of the {@link BoundTemplate}
+         * will be evaluated.
+         */
         TAG(TagVisitor::new),
+        /**
+         * Render style for erased types. Only the root of the
+         * {@link BoundTemplate} will be evaluated.
+         */
         ERASED(ErasedVisitor::new);
 
         private Style(Function<Config, BoundTemplate.Visitor<String>> visitor) {
             this.visitor = visitor;
         }
 
+        /**
+         * Retrieve the render implementation with a given configuration.
+         *
+         * @param config The configuration for the render operation.
+         * @return A visitor that renders the {@link BoundTemplate}.
+         */
         @Override
         public BoundTemplate.Visitor<String> apply(Config config) {
             return visitor.apply(config);
         }
 
+        /**
+         * Visitor factory.
+         */
         private final Function<Config, BoundTemplate.Visitor<String>> visitor;
+    }
 
-        private static class TagVisitor implements BoundTemplate.Visitor<String> {
-            public TagVisitor(Config config) {
-                this.config = requireNonNull(config);
+    private static class TagVisitor implements BoundTemplate.Visitor<String> {
+        public TagVisitor(Config config) {
+            this.config = requireNonNull(config);
+        }
+
+        @Override
+        public String apply(BoundTemplate.VarBinding b) {
+            throw new UnsupportedOperationException("Variable can not be converted to tag.");
+        }
+
+        @Override
+        public String apply(BoundTemplate.ClassBinding b) {
+            return "::java::_tags"
+                    + b.getType().getNamespace().stream().map(nsElem -> "::" + nsElem).collect(Collectors.joining())
+                    + "::"
+                    + b.getType().getSimpleName();
+        }
+
+        @Override
+        public String apply(BoundTemplate.ArrayBinding b) {
+            throw new UnsupportedOperationException("Arrays can not be converted to tag.");
+        }
+
+        @Override
+        public String apply(BoundTemplate.Any b) {
+            throw new UnsupportedOperationException("Unbound template variable can not be converted to tag.");
+        }
+
+        private final Config config;
+    }
+
+    private static class ErasedVisitor implements BoundTemplate.Visitor<String> {
+        public ErasedVisitor(Config config) {
+            this.config = requireNonNull(config);
+        }
+
+        @Override
+        public String apply(BoundTemplate.VarBinding b) {
+            throw new UnsupportedOperationException("Variable can not be converted to erased type.");
+        }
+
+        @Override
+        public String apply(BoundTemplate.ClassBinding b) {
+            return "::java::_erased"
+                    + b.getType().getNamespace().stream().map(nsElem -> "::" + nsElem).collect(Collectors.joining())
+                    + "::"
+                    + b.getType().getSimpleName();
+        }
+
+        @Override
+        public String apply(BoundTemplate.ArrayBinding b) {
+            throw new UnsupportedOperationException("Arrays can not be converted to erased type.");
+        }
+
+        @Override
+        public String apply(BoundTemplate.Any b) {
+            throw new UnsupportedOperationException("Unbound template variable can not be converted to erased type.");
+        }
+
+        private final Config config;
+    }
+
+    private static class TypeVisitor implements BoundTemplate.Visitor<String> {
+        public TypeVisitor(Config config) {
+            this.config = requireNonNull(config);
+            final Config rawConfig = Config.typeWithBase(config.base);
+            if (config.classType || Objects.equals(rawConfig, config))
+                this.raw = this;
+            else
+                this.raw = new TypeVisitor(rawConfig);
+        }
+
+        @Override
+        public String apply(BoundTemplate.VarBinding b) {
+            if (config.classType)
+                return "::java::type<" + b.getName() + ">";
+            if (config.accessorType)
+                throw new IllegalArgumentException("Variables do not have accessors.");
+            return b.getName();
+        }
+
+        @Override
+        public String apply(BoundTemplate.ClassBinding b) {
+            final String clsType = b.getType().getNamespace().stream().map(nsElem -> "::" + nsElem).collect(Collectors.joining())
+                    + "::"
+                    + b.getType().getSimpleName();
+
+            final Stream<String> bindings = b.getBindings().stream()
+                    .map(tmpl -> tmpl.visit(raw));
+
+            if (config.classType) {
+                if (b.getBindings().isEmpty()) return clsType;
+                return clsType + bindings.collect(Collectors.joining(", ", "<", ">"));
             }
 
-            @Override
-            public String apply(BoundTemplate.VarBinding b) {
-                throw new UnsupportedOperationException("Variable can not be converted to tag.");
+            final String tagName = "::java::_tags" + clsType;
+
+            if (config.accessorType) {
+                return "::java::_accessor" + Stream.concat(Stream.of(config.base, tagName), bindings).collect(Collectors.joining(", ", "<", ">"));
+            } else {
+                return "::java::G::is" + Stream.concat(Stream.of(tagName), bindings).collect(Collectors.joining(", ", "<", ">"));
+            }
+        }
+
+        @Override
+        public String apply(BoundTemplate.ArrayBinding b) {
+            final String elementType;
+            if (b.getType() instanceof BoundTemplate.ClassBinding && ((BoundTemplate.ClassBinding) b.getType()).getType() instanceof PrimitiveType) {
+                final PrimitiveType primitiveType = (PrimitiveType) ((BoundTemplate.ClassBinding) b.getType()).getType();
+                elementType = Stream.concat(primitiveType.getNamespace().stream(), Stream.of(primitiveType.getSimpleName()))
+                        .map(nameElem -> "::" + nameElem)
+                        .collect(Collectors.joining());
+            } else {
+                elementType = b.getType().visit(raw);
             }
 
-            @Override
-            public String apply(BoundTemplate.ClassBinding b) {
-                return "::java::_tags"
-                        + b.getType().getNamespace().stream().map(nsElem -> "::" + nsElem).collect(Collectors.joining())
-                        + "::"
-                        + b.getType().getSimpleName();
-            }
+            final String arrayObj = "::java::array_type<"
+                    + elementType
+                    + ", "
+                    + b.getExtents()
+                    + ">";
 
-            @Override
-            public String apply(BoundTemplate.ArrayBinding b) {
-                throw new UnsupportedOperationException("Arrays can not be converted to tag.");
-            }
+            if (config.classType) return arrayObj;
+            if (config.accessorType)
+                throw new IllegalArgumentException("Arrays do not have accessors.");
+            return "::java::type_of<" + arrayObj + ">";
+        }
 
-            @Override
-            public String apply(BoundTemplate.Any b) {
-                throw new UnsupportedOperationException("Unbound template variable can not be converted to tag.");
-            }
+        @Override
+        public String apply(BoundTemplate.Any b) {
+            final Stream<String> extendStream = b.getExtendTypes().stream()
+                    .map(type -> type.visit(raw))
+                    .map(typeStr -> "::java::G::extends<" + typeStr + ">");
+            final Stream<String> superStream = b.getSuperTypes().stream()
+                    .map(type -> type.visit(raw))
+                    .map(typeStr -> "::java::G::super<" + typeStr + ">");
+            final String type = "::java::G::pack"
+                    + Stream.concat(extendStream, superStream).collect(Collectors.joining(", ", "<", ">"));
 
-            private final Config config;
-        };
+            if (config.classType) return "::java::type<" + type + ">";
+            if (config.accessorType)
+                throw new IllegalArgumentException("Unbound typres do not have accessors.");
+            return type;
+        }
 
-        private static class ErasedVisitor implements BoundTemplate.Visitor<String> {
-            public ErasedVisitor(Config config) {
-                this.config = requireNonNull(config);
-            }
-
-            @Override
-            public String apply(BoundTemplate.VarBinding b) {
-                throw new UnsupportedOperationException("Variable can not be converted to erased type.");
-            }
-
-            @Override
-            public String apply(BoundTemplate.ClassBinding b) {
-                return "::java::_erased"
-                        + b.getType().getNamespace().stream().map(nsElem -> "::" + nsElem).collect(Collectors.joining())
-                        + "::"
-                        + b.getType().getSimpleName();
-            }
-
-            @Override
-            public String apply(BoundTemplate.ArrayBinding b) {
-                throw new UnsupportedOperationException("Arrays can not be converted to erased type.");
-            }
-
-            @Override
-            public String apply(BoundTemplate.Any b) {
-                throw new UnsupportedOperationException("Unbound template variable can not be converted to erased type.");
-            }
-
-            private final Config config;
-        };
-
-        private static class TypeVisitor implements BoundTemplate.Visitor<String> {
-            public TypeVisitor(Config config) {
-                this.config = requireNonNull(config);
-                final Config rawConfig = Config.typeWithBase(config.base);
-                if (config.classType || Objects.equals(rawConfig, config))
-                    this.raw = this;
-                else
-                    this.raw = new TypeVisitor(rawConfig);
-            }
-
-            @Override
-            public String apply(BoundTemplate.VarBinding b) {
-                if (config.classType)
-                    return "::java::type<" + b.getName() + ">";
-                if (config.accessorType)
-                    throw new IllegalArgumentException("Variables do not have accessors.");
-                return b.getName();
-            }
-
-            @Override
-            public String apply(BoundTemplate.ClassBinding b) {
-                final String clsType = b.getType().getNamespace().stream().map(nsElem -> "::" + nsElem).collect(Collectors.joining())
-                        + "::"
-                        + b.getType().getSimpleName();
-
-                final Stream<String> bindings = b.getBindings().stream()
-                        .map(tmpl -> tmpl.visit(raw));
-
-                if (config.classType) {
-                    if (b.getBindings().isEmpty()) return clsType;
-                    return clsType + bindings.collect(Collectors.joining(", ", "<", ">"));
-                }
-
-                final String tagName = "::java::_tags" + clsType;
-
-                if (config.accessorType) {
-                    return "::java::_accessor" + Stream.concat(Stream.of(config.base, tagName), bindings).collect(Collectors.joining(", ", "<", ">"));
-                } else {
-                    return "::java::G::is" + Stream.concat(Stream.of(tagName), bindings).collect(Collectors.joining(", ", "<", ">"));
-                }
-            }
-
-            @Override
-            public String apply(BoundTemplate.ArrayBinding b) {
-                final String elementType;
-                if (b.getType() instanceof BoundTemplate.ClassBinding && ((BoundTemplate.ClassBinding) b.getType()).getType() instanceof PrimitiveType) {
-                    final PrimitiveType primitiveType = (PrimitiveType) ((BoundTemplate.ClassBinding) b.getType()).getType();
-                    elementType = Stream.concat(primitiveType.getNamespace().stream(), Stream.of(primitiveType.getSimpleName()))
-                            .map(nameElem -> "::" + nameElem)
-                            .collect(Collectors.joining());
-                } else {
-                    elementType = b.getType().visit(raw);
-                }
-
-                final String arrayObj = "::java::array_type<"
-                        + elementType
-                        + ", "
-                        + b.getExtents()
-                        + ">";
-
-                if (config.classType) return arrayObj;
-                if (config.accessorType)
-                    throw new IllegalArgumentException("Arrays do not have accessors.");
-                return "::java::type_of<" + arrayObj + ">";
-            }
-
-            @Override
-            public String apply(BoundTemplate.Any b) {
-                final Stream<String> extendStream = b.getExtendTypes().stream()
-                        .map(type -> type.visit(raw))
-                        .map(typeStr -> "::java::G::extends<" + typeStr + ">");
-                final Stream<String> superStream = b.getSuperTypes().stream()
-                        .map(type -> type.visit(raw))
-                        .map(typeStr -> "::java::G::super<" + typeStr + ">");
-                final String type = "::java::G::pack"
-                        + Stream.concat(extendStream, superStream).collect(Collectors.joining(", ", "<", ">"));
-
-                if (config.classType) return "::java::type<" + type + ">";
-                if (config.accessorType)
-                    throw new IllegalArgumentException("Unbound typres do not have accessors.");
-                return type;
-            }
-
-            private final Config config;
-            private final TypeVisitor raw;
-        };
+        private final Config config;
+        private final TypeVisitor raw;
     }
 }
